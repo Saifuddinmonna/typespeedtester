2573b3e608f0b8b9643232f672016c8279755abf
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-09-15 21:38:47 +0600

    final version for assignment submitting 9:30

--- history.js
+++ history.js
@@ -6,12 +6,12 @@ function addHistory(questionText, timeTaken, errorCount) {
 
 	newRow.innerHTML = `
   <div>
-    <h4>${questionText}</h4>
+    <h5>${questionText}</h5>
   </div>
 
   <div>
    <p>
-    You took: <span class="bold">${timeTaken}</span> seconds</p>
+    You took : <span class="bold">${timeTaken}</span> seconds</p>
    </div>
   <div>
     <p>You made <span class="bold red">${errorCount}</span>  mistakes</p>
@@ -39,12 +39,11 @@ function displayHistory() {
 
 		newRow.innerHTML = `
   <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold">${test.timeTaken} </span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
 		histories.appendChild(newRow);
-  });
-  errorCount = "0";
+	});
+	errorCount = "0";
 }
-
